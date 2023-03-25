08ac0125d125fac606591be9adf62c8bbe5ed5bd
Halil Olcay <holcay92@gmail.com>
2023-03-25 18:11:00 +0300

    Splash Activity is added

---
+++ app/src/main/java/com/example/projectmanager/SplashActivity.kt
@@ -0,0 +1,40 @@
+package com.example.projectmanager
+
+
+import android.graphics.Typeface
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import com.example.projectmanager.databinding.ActivitySplashBinding
+
+
+class SplashActivity : AppCompatActivity() {
+    private var binding : ActivitySplashBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        // This is used to hide the status bar and make the splash screen as a full screen activity.
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        // This is used to get the file from the assets folder and set it to the title textView.
+       /* val typeface: Typeface =
+            Typeface.createFromAsset(assets, "carbon bl.ttf")
+            binding?.tvAppName?.typeface = typeface
+            TODO add a font to project
+            */
+
+    }
+}

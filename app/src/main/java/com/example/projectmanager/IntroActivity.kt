7e9b133ea07fc845b7bea2160af15c082556877e
Halil Olcay <holcay92@gmail.com>
2023-03-25 20:55:26 +0300

    signUp screen is implemented

---
+++ app/src/main/java/com/example/projectmanager/IntroActivity.kt
@@ -0,0 +1,38 @@
+package com.example.projectmanager
+
+import android.content.Intent
+import android.graphics.Typeface
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.example.projectmanager.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+    private var binding : ActivityIntroBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+       /* val typeface: Typeface =
+            Typeface.createFromAsset(assets, "carbon bl.ttf")
+        binding?.tvAppNameIntro?.typeface = typeface
+        TODO */
+        binding?.btnSignUpIntro?.setOnClickListener {
+            // Launch the sign up screen.
+            startActivity(Intent(this@IntroActivity, SignUpActivity::class.java))
+        }
+
+    }
+}
--- app/src/main/java/com/example/projectmanager/SplashActivity.kt
+++ app/src/main/java/com/example/projectmanager/SplashActivity.kt
@@ -1,10 +1,13 @@
 package com.example.projectmanager
 
 
+import android.content.Intent
 import android.graphics.Typeface
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.WindowInsets
 import android.view.WindowInsetsController
 import android.view.WindowManager
@@ -28,13 +31,18 @@ class SplashActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
-
         // This is used to get the file from the assets folder and set it to the title textView.
        /* val typeface: Typeface =
             Typeface.createFromAsset(assets, "carbon bl.ttf")
             binding?.tvAppName?.typeface = typeface
             TODO add a font to project
             */
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Start the Intro Activity
+            startActivity(Intent(this@SplashActivity, IntroActivity::class.java))
+            finish() // Call this when your activity is done and should be closed.
+        }, 500)
 
     }
 }

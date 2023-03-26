142357a1590d6f5592cafe263da75bec83d35fc1
Halil Olcay <holcay92@gmail.com>
2023-03-26 18:38:35 +0300

    profile activity binding bug is solved

--- app/src/main/java/com/example/projectmanager/activities/MyProfileActivity.kt
+++ app/src/main/java/com/example/projectmanager/activities/MyProfileActivity.kt
@@ -10,20 +10,17 @@ class MyProfileActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMyProfileBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_my_profile)
+        setContentView(binding?.root)
         setupActionBar()
     }
     private fun setupActionBar() {
-
         setSupportActionBar(binding?.toolbarMyProfileActivity)
-
         val actionBar = supportActionBar
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true)
             actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
             actionBar.title = resources.getString(R.string.my_profile)
         }
-
         binding?.toolbarMyProfileActivity?.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed() }
     }

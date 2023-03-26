0676f6e2b5475c5c7a3d787def0b99484a68f864
Halil Olcay <holcay92@gmail.com>
2023-03-26 20:45:30 +0300

    In the profile photo selection functionality is added

--- app/src/main/java/com/example/projectmanager/activities/MyProfileActivity.kt
+++ app/src/main/java/com/example/projectmanager/activities/MyProfileActivity.kt
@@ -1,6 +1,14 @@
 package com.example.projectmanager.activities
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.projectmanager.R
 import com.example.projectmanager.databinding.ActivityMyProfileBinding
@@ -9,6 +17,13 @@ import com.example.projectmanager.modals.User
 
 class MyProfileActivity : BaseActivity() {
     private var binding: ActivityMyProfileBinding? = null
+    companion object {
+        private const val READ_STORAGE_PERMISSION_CODE = 1
+        private const val PICK_IMAGE_REQUEST_CODE = 2
+    }
+
+    private var mSelectedImageFileUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMyProfileBinding.inflate(layoutInflater)
@@ -16,6 +31,72 @@ class MyProfileActivity : BaseActivity() {
         setupActionBar()
 
         FireStoreClass().loadUserData(this)
+
+        binding?.ivProfileUserImage?.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                showImageChooser()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    READ_STORAGE_PERMISSION_CODE
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == READ_STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showImageChooser()
+            } else {
+                Toast.makeText(
+                    this,
+                    "You just denied the storage permission for this app. You can allow it from the settings.",
+                    Toast.LENGTH_LONG
+                ).show()
+
+            }
+        }
+    }
+    private fun showImageChooser() {
+        val galleryIntent = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK&&requestCode == PICK_IMAGE_REQUEST_CODE) {
+                if (data != null) {
+                    try {
+                        mSelectedImageFileUri = data.data!!
+                        Glide
+                            .with(this)
+                            .load(mSelectedImageFileUri)
+                            .centerCrop()
+                            .placeholder(R.drawable.ic_user_place_holder)
+                            .into(binding?.ivProfileUserImage!!)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        Toast.makeText(
+                            this,
+                            resources.getString(R.string.image_selection_failed),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+
+        } else if (resultCode == RESULT_CANCELED) {
+            println("Image selection cancelled")
+        }
     }
 
     private fun setupActionBar() {
@@ -32,7 +113,7 @@ class MyProfileActivity : BaseActivity() {
     }
 
     fun setUserDataInUI(user: User) {
-        binding?.ivUserImage?.let {
+        binding?.ivProfileUserImage?.let {
             Glide
                 .with(this@MyProfileActivity)
                 .load(user.image)

86b9ec172d9513cc14b1a233fd8072adbcf3b82c
Halil Olcay <holcay92@gmail.com>
2023-03-26 16:09:26 +0300

    navigation click listener added to main activity and log out feat is added

--- app/src/main/java/com/example/projectmanager/activities/MainActivity.kt
+++ app/src/main/java/com/example/projectmanager/activities/MainActivity.kt
@@ -1,14 +1,73 @@
 package com.example.projectmanager.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import android.window.OnBackInvokedDispatcher
+import androidx.activity.OnBackPressedDispatcher
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.projectmanager.R
+import com.example.projectmanager.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity(),NavigationView.OnNavigationItemSelectedListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        setContentView(R.layout.activity_main)
+        setupActionBar()
+        findViewById<NavigationView>(R.id.nav_view).setNavigationItemSelectedListener(this)
     }
+
+    private fun setupActionBar() {
+        var toolbar = findViewById<Toolbar>(R.id.toolbar_main_activity)
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationIcon(R.drawable.ic_action_navigation_menu)
+        toolbar.setNavigationOnClickListener {
+            toggleDrawer()
+        }
+    }
+
+    private fun toggleDrawer() {
+        if (findViewById<DrawerLayout>(R.id.drawer_layout).isDrawerOpen(GravityCompat.START)) {
+            findViewById<DrawerLayout>(R.id.drawer_layout).closeDrawer(GravityCompat.START)
+        } else {
+            findViewById<DrawerLayout>(R.id.drawer_layout).openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (findViewById<DrawerLayout>(R.id.drawer_layout).isDrawerOpen(GravityCompat.START)) {
+            findViewById<DrawerLayout>(R.id.drawer_layout).closeDrawer(GravityCompat.START)
+        } else {
+            doubleBackToExit()
+
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_my_profile -> {
+               Toast.makeText(this@MainActivity,"My Profile",Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_sign_out -> {
+               Toast.makeText(this@MainActivity,"Sign Out",Toast.LENGTH_SHORT).show()
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this@MainActivity, IntroActivity::class.java)
+                // Clear all the activities and start new task or else it will open the previous activity.
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+                finish()
+            }
+        }
+        findViewById<DrawerLayout>(R.id.drawer_layout).closeDrawer(GravityCompat.START)
+        return true
+    }
+
 }
